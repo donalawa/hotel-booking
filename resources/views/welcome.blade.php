82b3cb75d9365d9cda7b732e1aa12f1f4ecd11c9
donalawa <awadonalcien12@gmail.com>
2022-12-31 17:27:02 +0100

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
+
+        <title>Gigs Hotel</title>
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="">
+        <div class="">
+            <h1 class="text-red-600">Hello World</h1>
+        </div>
+    </body>
+</html>
